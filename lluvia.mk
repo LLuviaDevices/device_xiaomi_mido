8ddfea6d4aad9e45b8b825bbd06a8bdfc36f5af8
AliHasan96 <admin@ialihasan.com>
2018-06-04 11:32:22 +0000

    Let it Rain

--- lluvia.mk
+++ lluvia.mk
@@ -16,10 +16,11 @@
 
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/lluvia/config/common_full_phone.mk)
+$(call inherit-product, vendor/lluvia/config/caf_fw.mk)
 
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := lluvia_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
